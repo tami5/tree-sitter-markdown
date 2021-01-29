5708f00ed44e64e724cbc0a80415bc7e380d1d3a
Tami <tamibam@yahoo.com>
2021-01-29 20:19:57 +0300

    update

--- grammar.js
+++ grammar.js
@@ -1,9 +1,21 @@
 module.exports = grammar({
   name: 'markdown',
-  extras: $ => [
-    /\s+/,
-  ],
+  extras: $ => [ /\s+/, ],
   rules: {
-    document: $ => 'hello'
+    document: $ => seq(repeat($._any)),
+    _any: $ => choice($.paragraph),
+    text: (_) => /[\w\s]+/,
+    bold: $ => prec(2, choice(
+      seq("**", $.text, "**"),
+      seq("__", $.text, "__"),
+    )),
+    paragraph: $ => choice(
+      $.bold,
+      // $.text
+    )
   }
 });
+
+// text: $ => token(prec.right($._txt)),
+// text: $ => token(prec.right(/[^<>]+/)),
+
--- src/parser.c
+++ src/parser.c
@@ -6,30 +6,48 @@
 #endif
 
 #define LANGUAGE_VERSION 12
-#define STATE_COUNT 4
-#define LARGE_STATE_COUNT 2
-#define SYMBOL_COUNT 3
+#define STATE_COUNT 11
+#define LARGE_STATE_COUNT 4
+#define SYMBOL_COUNT 9
 #define ALIAS_COUNT 0
-#define TOKEN_COUNT 2
+#define TOKEN_COUNT 4
 #define EXTERNAL_TOKEN_COUNT 0
 #define FIELD_COUNT 0
-#define MAX_ALIAS_SEQUENCE_LENGTH 1
+#define MAX_ALIAS_SEQUENCE_LENGTH 3
 
 enum {
-  anon_sym_hello = 1,
-  sym_document = 2,
+  sym_text = 1,
+  anon_sym_STAR_STAR = 2,
+  anon_sym___ = 3,
+  sym_document = 4,
+  sym__any = 5,
+  sym_bold = 6,
+  sym_paragraph = 7,
+  aux_sym_document_repeat1 = 8,
 };
 
 static const char *ts_symbol_names[] = {
   [ts_builtin_sym_end] = "end",
-  [anon_sym_hello] = "hello",
+  [sym_text] = "text",
+  [anon_sym_STAR_STAR] = "**",
+  [anon_sym___] = "__",
   [sym_document] = "document",
+  [sym__any] = "_any",
+  [sym_bold] = "bold",
+  [sym_paragraph] = "paragraph",
+  [aux_sym_document_repeat1] = "document_repeat1",
 };
 
 static TSSymbol ts_symbol_map[] = {
   [ts_builtin_sym_end] = ts_builtin_sym_end,
-  [anon_sym_hello] = anon_sym_hello,
+  [sym_text] = sym_text,
+  [anon_sym_STAR_STAR] = anon_sym_STAR_STAR,
+  [anon_sym___] = anon_sym___,
   [sym_document] = sym_document,
+  [sym__any] = sym__any,
+  [sym_bold] = sym_bold,
+  [sym_paragraph] = sym_paragraph,
+  [aux_sym_document_repeat1] = aux_sym_document_repeat1,
 };
 
 static const TSSymbolMetadata ts_symbol_metadata[] = {
@@ -37,13 +55,37 @@ static const TSSymbolMetadata ts_symbol_metadata[] = {
     .visible = false,
     .named = true,
   },
-  [anon_sym_hello] = {
+  [sym_text] = {
+    .visible = true,
+    .named = true,
+  },
+  [anon_sym_STAR_STAR] = {
+    .visible = true,
+    .named = false,
+  },
+  [anon_sym___] = {
     .visible = true,
     .named = false,
   },
   [sym_document] = {
     .visible = true,
     .named = true,
+  },
+  [sym__any] = {
+    .visible = false,
+    .named = true,
+  },
+  [sym_bold] = {
+    .visible = true,
+    .named = true,
+  },
+  [sym_paragraph] = {
+    .visible = true,
+    .named = true,
+  },
+  [aux_sym_document_repeat1] = {
+    .visible = false,
+    .named = false,
   },
 };
 
@@ -60,30 +102,60 @@ static bool ts_lex(TSLexer *lexer, TSStateId state) {
   eof = lexer->eof(lexer);
   switch (state) {
     case 0:
-      if (eof) ADVANCE(5);
-      if (lookahead == 'h') ADVANCE(1);
+      if (eof) ADVANCE(4);
+      if (lookahead == '*') ADVANCE(1);
+      if (lookahead == '_') ADVANCE(2);
       if (lookahead == '\t' ||
           lookahead == '\n' ||
           lookahead == '\r' ||
           lookahead == ' ') SKIP(0)
       END_STATE();
     case 1:
-      if (lookahead == 'e') ADVANCE(3);
+      if (lookahead == '*') ADVANCE(7);
       END_STATE();
     case 2:
-      if (lookahead == 'l') ADVANCE(4);
+      if (lookahead == '_') ADVANCE(8);
       END_STATE();
     case 3:
-      if (lookahead == 'l') ADVANCE(2);
+      if (lookahead == '\t' ||
+          lookahead == '\n' ||
+          lookahead == '\r' ||
+          lookahead == ' ') ADVANCE(5);
+      if (('0' <= lookahead && lookahead <= '9') ||
+          ('A' <= lookahead && lookahead <= 'Z') ||
+          lookahead == '_' ||
+          ('a' <= lookahead && lookahead <= 'z')) ADVANCE(6);
       END_STATE();
     case 4:
-      if (lookahead == 'o') ADVANCE(6);
-      END_STATE();
-    case 5:
       ACCEPT_TOKEN(ts_builtin_sym_end);
       END_STATE();
+    case 5:
+      ACCEPT_TOKEN(sym_text);
+      if (lookahead == '\t' ||
+          lookahead == '\n' ||
+          lookahead == '\r' ||
+          lookahead == ' ') ADVANCE(5);
+      if (('0' <= lookahead && lookahead <= '9') ||
+          ('A' <= lookahead && lookahead <= 'Z') ||
+          lookahead == '_' ||
+          ('a' <= lookahead && lookahead <= 'z')) ADVANCE(6);
+      END_STATE();
     case 6:
-      ACCEPT_TOKEN(anon_sym_hello);
+      ACCEPT_TOKEN(sym_text);
+      if (lookahead == '\t' ||
+          lookahead == '\n' ||
+          lookahead == '\r' ||
+          lookahead == ' ' ||
+          ('0' <= lookahead && lookahead <= '9') ||
+          ('A' <= lookahead && lookahead <= 'Z') ||
+          lookahead == '_' ||
+          ('a' <= lookahead && lookahead <= 'z')) ADVANCE(6);
+      END_STATE();
+    case 7:
+      ACCEPT_TOKEN(anon_sym_STAR_STAR);
+      END_STATE();
+    case 8:
+      ACCEPT_TOKEN(anon_sym___);
       END_STATE();
     default:
       return false;
@@ -95,39 +167,105 @@ static TSLexMode ts_lex_modes[STATE_COUNT] = {
   [1] = {.lex_state = 0},
   [2] = {.lex_state = 0},
   [3] = {.lex_state = 0},
+  [4] = {.lex_state = 0},
+  [5] = {.lex_state = 0},
+  [6] = {.lex_state = 3},
+  [7] = {.lex_state = 3},
+  [8] = {.lex_state = 0},
+  [9] = {.lex_state = 0},
+  [10] = {.lex_state = 0},
 };
 
 static uint16_t ts_parse_table[LARGE_STATE_COUNT][SYMBOL_COUNT] = {
   [0] = {
     [ts_builtin_sym_end] = ACTIONS(1),
-    [anon_sym_hello] = ACTIONS(1),
+    [anon_sym_STAR_STAR] = ACTIONS(1),
+    [anon_sym___] = ACTIONS(1),
   },
   [1] = {
-    [sym_document] = STATE(3),
-    [anon_sym_hello] = ACTIONS(3),
+    [sym_document] = STATE(8),
+    [sym__any] = STATE(2),
+    [sym_bold] = STATE(4),
+    [sym_paragraph] = STATE(2),
+    [aux_sym_document_repeat1] = STATE(2),
+    [ts_builtin_sym_end] = ACTIONS(3),
+    [anon_sym_STAR_STAR] = ACTIONS(5),
+    [anon_sym___] = ACTIONS(7),
+  },
+  [2] = {
+    [sym__any] = STATE(3),
+    [sym_bold] = STATE(4),
+    [sym_paragraph] = STATE(3),
+    [aux_sym_document_repeat1] = STATE(3),
+    [ts_builtin_sym_end] = ACTIONS(9),
+    [anon_sym_STAR_STAR] = ACTIONS(5),
+    [anon_sym___] = ACTIONS(7),
+  },
+  [3] = {
+    [sym__any] = STATE(3),
+    [sym_bold] = STATE(4),
+    [sym_paragraph] = STATE(3),
+    [aux_sym_document_repeat1] = STATE(3),
+    [ts_builtin_sym_end] = ACTIONS(11),
+    [anon_sym_STAR_STAR] = ACTIONS(13),
+    [anon_sym___] = ACTIONS(16),
   },
 };
 
 static uint16_t ts_small_parse_table[] = {
   [0] = 1,
-    ACTIONS(5), 1,
+    ACTIONS(19), 3,
       ts_builtin_sym_end,
-  [4] = 1,
-    ACTIONS(7), 1,
+      anon_sym_STAR_STAR,
+      anon_sym___,
+  [6] = 1,
+    ACTIONS(21), 3,
       ts_builtin_sym_end,
+      anon_sym_STAR_STAR,
+      anon_sym___,
+  [12] = 1,
+    ACTIONS(23), 1,
+      sym_text,
+  [16] = 1,
+    ACTIONS(25), 1,
+      sym_text,
+  [20] = 1,
+    ACTIONS(27), 1,
+      ts_builtin_sym_end,
+  [24] = 1,
+    ACTIONS(29), 1,
+      anon_sym_STAR_STAR,
+  [28] = 1,
+    ACTIONS(29), 1,
+      anon_sym___,
 };
 
 static uint32_t ts_small_parse_table_map[] = {
-  [SMALL_STATE(2)] = 0,
-  [SMALL_STATE(3)] = 4,
+  [SMALL_STATE(4)] = 0,
+  [SMALL_STATE(5)] = 6,
+  [SMALL_STATE(6)] = 12,
+  [SMALL_STATE(7)] = 16,
+  [SMALL_STATE(8)] = 20,
+  [SMALL_STATE(9)] = 24,
+  [SMALL_STATE(10)] = 28,
 };
 
 static TSParseActionEntry ts_parse_actions[] = {
   [0] = {.entry = {.count = 0, .reusable = false}},
   [1] = {.entry = {.count = 1, .reusable = false}}, RECOVER(),
-  [3] = {.entry = {.count = 1, .reusable = true}}, SHIFT(2),
-  [5] = {.entry = {.count = 1, .reusable = true}}, REDUCE(sym_document, 1),
-  [7] = {.entry = {.count = 1, .reusable = true}},  ACCEPT_INPUT(),
+  [3] = {.entry = {.count = 1, .reusable = true}}, REDUCE(sym_document, 0),
+  [5] = {.entry = {.count = 1, .reusable = true}}, SHIFT(6),
+  [7] = {.entry = {.count = 1, .reusable = true}}, SHIFT(7),
+  [9] = {.entry = {.count = 1, .reusable = true}}, REDUCE(sym_document, 1),
+  [11] = {.entry = {.count = 1, .reusable = true}}, REDUCE(aux_sym_document_repeat1, 2),
+  [13] = {.entry = {.count = 2, .reusable = true}}, REDUCE(aux_sym_document_repeat1, 2), SHIFT_REPEAT(6),
+  [16] = {.entry = {.count = 2, .reusable = true}}, REDUCE(aux_sym_document_repeat1, 2), SHIFT_REPEAT(7),
+  [19] = {.entry = {.count = 1, .reusable = true}}, REDUCE(sym_paragraph, 1),
+  [21] = {.entry = {.count = 1, .reusable = true}}, REDUCE(sym_bold, 3),
+  [23] = {.entry = {.count = 1, .reusable = true}}, SHIFT(9),
+  [25] = {.entry = {.count = 1, .reusable = true}}, SHIFT(10),
+  [27] = {.entry = {.count = 1, .reusable = true}},  ACCEPT_INPUT(),
+  [29] = {.entry = {.count = 1, .reusable = true}}, SHIFT(5),
 };
 
 #ifdef __cplusplus
